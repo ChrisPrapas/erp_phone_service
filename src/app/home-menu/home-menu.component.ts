ec248f673b326eb53b38bdc516b5d3dc5c6f581b
CHRISTOS PRAPAS <chris.l.prapas@gmail.com>
2023-09-12 18:37:14 +0300

    Continuing to develop home page with customer and phone addition

--- src/app/home-menu/home-menu.component.ts
+++ src/app/home-menu/home-menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-menu',
   templateUrl: './home-menu.component.html',
   styleUrls: ['./home-menu.component.css']
 })
-export class HomeMenuComponent {
+export class HomeMenuComponent implements OnInit {
 
+  @Input()
+  classApplied1 : boolean = false;
+
+  constructor() {
+    
+  }
+  
+  ngOnInit(): void {
+    
+  }
 }
